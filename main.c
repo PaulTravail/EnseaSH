892f48100a2def6e55f80255814377f4c33e61c3
ensea <paul.boulot@ensea.fr>
2023-11-29 16:05:51 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    // const char *message;
+    int fd = 1;
+
+    //message d'accueil
+    const char *message = "Bienvenue dans le Shell ENSEA. \n";
+    write(fd, message, strlen(message));
+    message = "Pour quitter, taper 'exit'. \n";
+    write(fd, message, strlen(message));
+    message = "enseash % \n";
+    write(fd,message,strlen(message));
+}
