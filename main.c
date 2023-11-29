004242a346e46b1fd31e8892d259d984d647ae95
ensea <paul.boulot@ensea.fr>
2023-11-29 19:37:18 +0100

    Question2

--- main.c
+++ main.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+#define BUFFSIZE 128
 
 int main(int argc, char *argv[]) {
-    // const char *message;
+
+    char command[BUFFSIZE];
+
+    //Welcome message
+    const char *message = "Bienvenue dans le Shell ENSEA. \n";
     int fd = 1;
 
-    //message d'accueil
-    const char *message = "Bienvenue dans le Shell ENSEA. \n";
     write(fd, message, strlen(message));
     message = "Pour quitter, taper 'exit'. \n";
     write(fd, message, strlen(message));
-    message = "enseash % \n";
+    message = "enseash % ";
     write(fd,message,strlen(message));
+
+    while(1) {
+
+        int input = read(fd,command,BUFFSIZE);
+
+        command[input-1]='\0';     //exclude the NULL character at the end of the table to read the command
+        if (input!=0) {
+            int pid=fork();        //creation of a new processus to do secondary tasks
+            if (pid!=0) {
+                sleep(1);
+            } else {
+                execlp(command,command,(char *)NULL);
+                exit(EXIT_FAILURE);     //Avoids opening several child processes in parallel
+            }
+        }
+        write(fd,message,strlen(message));
+    }
 }
+
+
