75e6dd1892eb4c65159fefcd69598966095c4769
ensea <paul.boulot@ensea.fr>
2023-12-06 17:30:47 +0100

    question5

--- TP1.c
+++ TP1.c
@@ -3,32 +3,36 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <time.h>
 
 #define BUFFSIZE 128
 #define fd 1
 #define WELCOME "Bienvenue dans le Shell ENSEA. \nPour quitter, taper 'exit'.\nenseash % "
 #define GOODBYE "Bye Bye ... \n"
-
+#define MILLION  1000000
 
 void prompt(char *message){
     write(fd, message, strlen(message));
 }
 
-void return_code(void){
+void return_code(int begin, int end){
     int status;
     wait(&status);
     char code_exit[BUFFSIZE];
     if (WIFEXITED(status)) {
-        sprintf(code_exit, "enseash [exit:%d] %% ", WEXITSTATUS(status));
+        sprintf(code_exit,"enseash[exit:%d|%.2lfms] %% ", WEXITSTATUS(status),(double)(end-begin)/MILLION);
         write(fd,code_exit,strlen(code_exit));
 
     } else if (WIFSIGNALED(status)) {
-        sprintf(code_exit, "enseash [sign:%d] %% ", WTERMSIG(status));
+        sprintf(code_exit,"enseash[exit:%d|%.2lfms] %% ", WEXITSTATUS(status),(double)(end-begin)/MILLION);
         write(fd,code_exit,strlen(code_exit));
     }
 }
 
 int main(int argc, char *argv[]) {
+
+    struct timespec startTime, endTime;
+    clock_t clk_id=CLOCK_REALTIME;
 
     char command[BUFFSIZE];
 
@@ -44,8 +48,9 @@ int main(int argc, char *argv[]) {
         command[input-1]='\0';     //exclude the NULL character at the end of the table to read the command
 
         if (input!=0) {
+            int beginClock = clock_gettime(clk_id, &startTime);
+            beginClock = startTime.tv_nsec;
             if (strcmp(command,"exit")==0) {
-
                 prompt(GOODBYE);
                 exit(EXIT_SUCCESS);}
             else {
@@ -57,8 +62,13 @@ int main(int argc, char *argv[]) {
                     execlp(command, command, (char *) NULL);
                     exit(EXIT_FAILURE);     //Avoids opening several child processes in parallel
                 }
-                return_code();
+                int endClock = clock_gettime(clk_id, &endTime);
+                endClock = endTime.tv_nsec;
+
+                return_code(beginClock,endClock);
             }
         }
     }
 }
+
+
